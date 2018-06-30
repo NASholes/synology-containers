7f9c120fed801ddeb0be90366dfe5cbaeb023d2b
Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>
2018-06-30 15:32:36 -0400

    caddy: configurable http(s) ports

--- svc/caddy.sh
+++ svc/caddy.sh
@@ -5,8 +5,8 @@ ACME_AGREE=false
 function run() {
   sudo docker run -d \
     -e "CADDYPATH=/etc/caddy" \
-    -p 8888:8888 \
-    -p 8887:8887 \
+    -p "$CADDY_HTTP_PORT:$CADDY_HTTP_PORT" \
+    -p "$CADDY_HTTPS_PORT:$CADDY_HTTPS_PORT" \
     -v "$CONFIG_DIR/$NAME:/etc/caddy:rw" \
     -v "$CONFIG_DIR/$NAME/Caddyfile:/etc/Caddyfile:ro" \
     --name "$NAME" \
@@ -16,6 +16,6 @@ function run() {
     --conf /etc/Caddyfile \
     --log stdout \
     --agree=$ACME_AGREE \
-    --http-port 8888 \
-    --https-port 8887
+    --http-port "$CADDY_HTTP_PORT" \
+    --https-port "$CADDY_HTTPS_PORT"
 }
