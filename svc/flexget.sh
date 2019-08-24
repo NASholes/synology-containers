3d81042241dcd567e85bbc38d54193159017bcb8
Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>
2019-08-24 03:19:43 -0400

    add groups to flexget

--- svc/flexget.sh
+++ svc/flexget.sh
@@ -7,6 +7,8 @@ function run() {
     -e "TORRENT_PLUGIN=$FLEXGET_TORRENT_PLUGIN" \
     -e "WEB_PASSWD=$FLEXGET_WEB_PASSWD" \
     -e "FLEXGET_LOG_LEVEL=verbose" \
+    -e "PUID=1027" \
+    -e "PGID=$DOCKER_GROUP_ID" \
     -p "$FLEXGET_PORT:5050" \
     -v "$CONFIG_DIR/$NAME:/config:rw" \
     -v "$DOWNLOAD_DIR:/downloads:rw" \
