7347ae35cfeee8ce1c1eff64cae1efca0306361e
Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>
2019-12-07 00:13:19 -0500

    kresd: add env

--- svc/kresd.sh
+++ svc/kresd.sh
@@ -4,14 +4,16 @@ IMAGE="cznic/knot-resolver:latest"
 function run() {
   sudo docker run -d \
     -e "PGID=$DOCKER_GROUP_ID" \
-    -p 53:53 \
-    -p 53:53/udp \
-    -p 853:853 \
-    -p 8053:8053 \
+    -p "$KRESD_DNS_PORT:53" \
+    -p "$KRESD_DNS_PORT:53/udp" \
+    -p "$KRESD_DNS_HTTP_PORT:443" \
+    -p "$KRESD_DNS_TLS_PORT:853" \
+    -p "$KRESD_HTTP_PORT:8053" \
     -v "$CONFIG_DIR/$NAME:/data:rw" \
     --name "$NAME" \
     --restart always \
     --net host \
     $IMAGE \
-    -c /data/config.lua
+    -c /data/config.lua \
+	-f 1
 }
